44461b643135e82bdc7238e0448b0302058342b3
Jacco Gritter <jacco.gritter@gmail.com>
2019-06-11 14:34:30 +0200

    deleted class methods - not good with json

--- bookClass.php
+++ bookClass.php
@@ -14,56 +14,56 @@ Class Book {
         $this->opmerkingen = $opmerkingen;
     }
 
-    // GETTERS
-    public function get_titel()
-	{
-		return $this->titel;
-    }
+    // // GETTERS
+    // public function get_titel()
+	// {
+	// 	return $this->titel;
+    // }
     
-    public function get_auteur()
-	{
-		return $this->auteur;
-    }
+    // public function get_auteur()
+	// {
+	// 	return $this->auteur;
+    // }
     
-    public function get_taal()
-	{
-		return $this->taal;
-    }
+    // public function get_taal()
+	// {
+	// 	return $this->taal;
+    // }
     
-    public function get_paginas()
-	{
-		return $this->paginas;
-	}
+    // public function get_paginas()
+	// {
+	// 	return $this->paginas;
+	// }
 
-    public function get_opmerkingen()
-	{
-		return $this->opmerkingen;
-    }
+    // public function get_opmerkingen()
+	// {
+	// 	return $this->opmerkingen;
+    // }
 
-    // SETTERS    
-    public function set_titel($titel)
-	{
-		$this->titel = $titel;
-    }
+    // // SETTERS    
+    // public function set_titel($titel)
+	// {
+	// 	$this->titel = $titel;
+    // }
     
-    public function set_auteur($auteur)
-	{
-		$this->auteur = $auteur;
-    }
+    // public function set_auteur($auteur)
+	// {
+	// 	$this->auteur = $auteur;
+    // }
     
-    public function set_taal($taal)
-	{
-		$this->taal = $taal;
-    }
+    // public function set_taal($taal)
+	// {
+	// 	$this->taal = $taal;
+    // }
     
-    public function set_paginas($paginas)
-	{
-		$this->paginas = $paginas;
-	}
+    // public function set_paginas($paginas)
+	// {
+	// 	$this->paginas = $paginas;
+	// }
 
-    public function set_opmerkingen()
-	{
-		return $this->opmerkingen;
-	}
+    // public function set_opmerkingen()
+	// {
+	// 	return $this->opmerkingen;
+	// }
 
 }
